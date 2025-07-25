e2626a1f0e241103f1e3b266a41c9ff2452ba8c3
Keshav sharma <135090664+obitox7@users.noreply.github.com>
2025-07-25 23:53:42 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -3,3 +3,19 @@ gsap.from("#body",{
     duration:1,
     
 })
+gsap.from("#text h1",{
+    x:"-100%",
+    delay:0.2,
+    duration:0.7
+})
+gsap.from("#p p",{
+    x:"150%",
+    delay:0.5,
+    duration:0.7
+})
+gsap.from("#t1",{
+    y:-100,
+    zIndex:-4,
+    delay:1,
+    duration:0.8
+})
